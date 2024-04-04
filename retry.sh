f43c2f582532f4c66c3e3e9a6473a2aa45be80c8
liberticl <francisco@liberti.cl>
2024-04-04 00:03:08 -0300

    feat(bash): automate refresh docker images for testing

---
+++ retry.sh
@@ -0,0 +1,7 @@
+echo "Eliminando todo lo que tiene docker"
+docker container prune
+docker images -f "dangling=true"
+docker rmi $(docker images -q)
+
+echo "Levantando el docker de Ciudades Pendientes"
+docker-compose up
