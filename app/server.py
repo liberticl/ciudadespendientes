cfa3cab712a6e7802ff22ce81a815bbfb75ee0ce
liberticl <francisco@liberti.cl>
2024-04-03 23:11:05 -0300

    feat(server): create requirements file and server start

--- app/server.py
+++ app/server.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from pymongo import MongoClient
-from settings import (MONGO_DB, MONGO_CP_DB, CP_STRAVA_COLLECTION)
+from settings import (DEBUG, MONGO_DB, MONGO_CP_DB, CP_STRAVA_COLLECTION)
 from utils import plot_data
 
 
 app = Flask(__name__)
 
 
-@app.route("/valpo")
+@app.route("/")
 def fullscreen():
     client = MongoClient(MONGO_DB)
     db = client[MONGO_CP_DB]
     collection = db[CP_STRAVA_COLLECTION]
     m = plot_data(collection)
     return m.get_root().render()
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, host='0.0.0.0', port=8080)
