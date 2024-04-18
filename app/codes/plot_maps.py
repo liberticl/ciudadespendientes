6751907ff4f3b0e72f2dfa7df43b7d8aef69fc39
liberticl <francisco@liberti.cl>
2024-04-18 00:24:43 -0400

    feat(server): started to test search elements. Not working now

--- app/codes/plot_maps.py
+++ app/codes/plot_maps.py
@@ -3,6 +3,7 @@ import folium
 from .apis import get_place_polygon
 from .mongodb import points_inside
 from shapely.geometry import LineString, Point, Polygon
+from shapely.geometry import box
 
 
 def ride_map(collection):
@@ -73,21 +74,27 @@ def city_map(city):
     return mapa
 
 
-def get_city_bounds(city):
+def get_city_bounds(city, only_bounds = False):
     gdf_city = get_place_polygon(city).to_crs('EPSG:3857')
     gdf_city['area'] = gdf_city['geometry'].area
     data = gdf_city[gdf_city['area'] == gdf_city['area'].max()]
-    data = data.to_crs(epsg='4326').to_dict()
-    return data['geometry'][0]
 
-from shapely.wkt import loads
-def city_ride_map(city, collection):
+    if(only_bounds):
+        data = data[['geometry']].to_crs(epsg='4326').total_bounds
+        return box(*data)
+    else:
+        data = data.to_crs(epsg='4326').to_dict()
+        return data['geometry'][0]
+
+
+def city_ride_map(city, center,collection):
     coords = list(Polygon(get_city_bounds(city)).exterior.coords)
     coords = [[round(x,6), round(y,6)] for x,y in coords]
     coords = [coords + [coords[0]]]
-    points_inside['middlePoint']['$geoWithin']['$geometry']['coordinates'] = coords
+    inside = points_inside
+    inside['middlePoint']['$geoWithin']['$geometry']['coordinates'] = coords
     projection = ['year', 'geometry']
-    cursor = collection.find(points_inside, {'_id': 0, **dict.fromkeys(projection, 1)})
+    cursor = collection.find(inside, {'_id': 0, **dict.fromkeys(projection, 1)})
 
     data = list(cursor)
     parsed_data = []
@@ -102,10 +109,68 @@ def city_ride_map(city, collection):
 
     gdf = gpd.GeoDataFrame(parsed_data)
 
-    mapa = folium.Map(location=(-33.049689, -71.621202),    # Mi casa
+    mapa = folium.Map(location= center,
                       zoom_start=13,    # Ver si es factible automatizar
                       control_scale=True
                       )
     geojson_data = gdf.to_json()
     folium.GeoJson(geojson_data).add_to(mapa)
+    return mapa
+
+
+def color_ride_map(city, center,collection):
+    coords = list(Polygon(get_city_bounds(city)).exterior.coords)
+    coords = [[round(x,6), round(y,6)] for x,y in coords]
+    coords = [coords + [coords[0]]]
+    inside = points_inside
+    inside['middlePoint']['$geoWithin']['$geometry']['coordinates'] = coords
+    projection = ['year', 'edgeUID', 'total_trip_count', 'geometry']
+    cursor = collection.find(inside, {'_id': 0, **dict.fromkeys(projection, 1)})
+
+    data = list(cursor)
+    parsed_data = []
+    for item in data:
+        parsed_proj = {}
+        for key in projection:
+            if key != 'geometry':
+                parsed_proj.update({key: item[key]})
+            else:
+                parsed_proj.update({key: LineString(item[key]['coordinates'])})
+        parsed_data.append(parsed_proj)
+
+    import matplotlib
+    matplotlib.use('agg')
+    gdf = gpd.GeoDataFrame(parsed_data)
+    # gdf.crs = 'EPSG:3857'
+
+    mapa = gdf.explore(
+        column='total_trip_count',
+        legend=True,
+        tooltip=False,
+        popup=['year','total_trip_count','edgeUID'],
+        legend_kwds=dict(colorbar=False),
+        name='MAPITA'
+    )
+
+    folium.TileLayer('openstreetmap', show=True).add_to(mapa)
+
+    folium.LayerControl().add_to(mapa)
+    # mapa = folium.Map(location= center,
+    #                   zoom_start=13,    # Ver si es factible automatizar
+    #                   control_scale=True
+    #                   )
+    
+    # geojson_data = gdf.to_json()
+    
+    # folium.Choropleth(
+    #     geo_data=geojson_data,
+    #     data=gdf,
+    #     columns=['geometry', 'total_trip_count'],
+    #     key_on='feature.geometry',
+    #     fill_color='YlGn',
+    #     fill_opacity=0.7,
+    #     line_opacity=0.2,
+    #     legend_name='Total Trip Count'
+    # ).add_to(mapa)
+
     return mapa
--- app/server.py
+++ app/server.py
@@ -1,20 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from pymongo import MongoClient
 from settings import (DEBUG, MONGO_DB, MONGO_CP_DB, CP_STRAVA_COLLECTION)
 from codes.plot_maps import *
+from utils import get_polygon_middle_point
 
 
 app = Flask(__name__)
+ALLOWED_CITIES = ['Viña del Mar', 'Valparaíso', 'Villa Alemana', 'Quilpué', 'Concón']
+city = 'Viña del Mar, Chile'
 
-
+# @app.route("/", methods=['GET', 'POST'])
 @app.route("/")
 def fullscreen():
+    # if request.method == 'POST':
+    #     city = request.form['city']
+    # else:
+    #     city = request.args.get('city', 'Viña del Mar')
+    # city += ', Chile'
     client = MongoClient(MONGO_DB)
     db = client[MONGO_CP_DB]
     collection = db[CP_STRAVA_COLLECTION]
-    # m = ride_map(collection)
-    m = city_ride_map('Viña del Mar, Chile', collection)
+    center = get_polygon_middle_point(city, collection)
+    m = color_ride_map(city, center, collection)
     return m.get_root().render()
+
+
+# @app.route("/search")
+# def search():
+#     return render_template('search.html', cities=ALLOWED_CITIES)
 
 
 if __name__ == '__main__':
