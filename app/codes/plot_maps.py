dd93d3aa57d3478d455cf2e137ce90282ae62223
Francisco Vergara <fvergara@drivetech.pro>
2024-04-26 00:26:00 -0400

    feat(server): created index and map views

--- app/codes/plot_maps.py
+++ app/codes/plot_maps.py
@@ -123,7 +123,74 @@ def city_ride_map(city, center, collection):
     return mapa
 
 
-def color_ride_map(city, center, years, collection1, collection2):
+def color_ride_map(city, center, years, collection1): #, collection2):
+    coords = list(Polygon(get_city_bounds(city)).exterior.coords)
+    coords = [[round(x, 6), round(y, 6)] for x, y in coords]
+    coords = [coords + [coords[0]]]
+    inside = points_inside[0]['$match']
+    inside['middlePoint']['$geoWithin']['$geometry']['coordinates'] = coords
+    inside['year']['$in'] = years
+    projection = ['year', 'total_trip_count', 'geometry']
+    del coords
+    cursor1 = collection1.find(
+        inside,
+        {'_id': 0, **dict.fromkeys(projection, 1)})
+
+    # cursor2 = collection2.find()
+
+    tile = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}' # noqa
+    attr ='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community' # noqa
+
+    mapa = folium.Map(
+        location=center,
+        zoom_start=13,
+        control_scale=True,
+        prefer_canvas=True,
+        tiles='')
+
+    folium.TileLayer(tile, name='Mapa', attr=attr).add_to(mapa)
+
+    color_layers = {'green': folium.FeatureGroup(name='Flujo bajo'),
+                    'orange': folium.FeatureGroup(name='Flujo medio'),
+                    'red': folium.FeatureGroup(name='Flujo alto'),
+                    # 'ascensores': folium.FeatureGroup(name='Ascensores'),
+                    }
+
+    # https://sni.gob.cl/storage/docs/Ciclo-Rutas-2013.pdf (pag 16)
+    def classify(trip_count, years, factor):
+        trip_count = trip_count / factor
+        by_day = trip_count / (365 * len(years))
+        if (by_day > 251):
+            return ['red', {"opacity": 0.9, "weight": 4}]
+        elif (by_day > 151):
+            return ['orange', {"opacity": 0.8, "weight": 3}]
+        else:
+            return ['green', {"opacity": 0.3, "weight": 3}]
+
+    for item in cursor1:
+        coords = [[lat, lon] for lon, lat in item['geometry']['coordinates']]
+        total_trip_count = item['total_trip_count']
+        classification = classify(total_trip_count, years, 0.01)
+        total_trip_count = '{:,.0f}'.format(
+            round(item['total_trip_count'] / 0.01)).replace(',', '.')
+
+        folium.PolyLine(locations=coords,
+                        color=classification[0],
+                        tooltip=f"Viajes anuales estimados: {total_trip_count}", # noqa
+                        **classification[1]).add_to(color_layers[classification[0]]) # noqa
+
+    for color_group in color_layers.values():
+        color_group.add_to(mapa)
+
+    folium.LayerControl(collapsed=False,
+                        overlay=True).add_to(mapa)
+
+    return mapa
+
+
+# Incluye visualización de ascensores
+# Crear función con  las capas
+def color_ride_map2(city, center, years, collection1, collection2):
     coords = list(Polygon(get_city_bounds(city)).exterior.coords)
     coords = [[round(x, 6), round(y, 6)] for x, y in coords]
     coords = [coords + [coords[0]]]
@@ -157,8 +224,9 @@ def color_ride_map(city, center, years, collection1, collection2):
                     }
 
     # https://sni.gob.cl/storage/docs/Ciclo-Rutas-2013.pdf (pag 16)
-    def classify(trip_count):
-        by_day = trip_count / 365
+    def classify(trip_count, years, factor):
+        trip_count = trip_count / factor
+        by_day = trip_count / (365 * len(years))
         if (by_day > 251):
             return ['red', {"opacity": 0.9, "weight": 4}]
         elif (by_day > 151):
@@ -168,10 +236,10 @@ def color_ride_map(city, center, years, collection1, collection2):
 
     for item in cursor1:
         coords = [[lat, lon] for lon, lat in item['geometry']['coordinates']]
-        total_trip_count = round(item['total_trip_count'] / 0.01)
-        classification = classify(total_trip_count)
+        total_trip_count = item['total_trip_count']
+        classification = classify(total_trip_count, years, factor)
         total_trip_count = '{:,.0f}'.format(
-            round(item['total_trip_count'] / 0.01)).replace(',', '.')
+            round(item['total_trip_count'] / 0.1)).replace(',', '.')
 
         folium.PolyLine(locations=coords,
                         color=classification[0],
--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, url_for, render_template
 from pymongo import MongoClient
 from settings import (DEBUG, MONGO_DB, MONGO_CP_DB, CP_STRAVA_COLLECTION)
 from codes.plot_maps import *
@@ -6,6 +6,8 @@ from utils import get_polygon_middle_point
 
 
 app = Flask(__name__)
+
+ALLOWED_YEARS = [2019, 2020, 2021, 2022, 2023]
 
 ALLOWED_CITIES = ['Viña del Mar',
                   'Valparaíso',
@@ -16,27 +18,41 @@ ALLOWED_CITIES = ['Viña del Mar',
 city = 'Valparaíso, Chile'
 
 
-# @app.route("/", methods=['GET', 'POST'])
-@app.route("/")
+# TO-DO: mejora de index.html
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        years = request.form.getlist("periodo")
+        cities = request.form.getlist("comunas")
+
+        return redirect(url_for("show_data", periodo=years, comunas=cities))
+    
+    return render_template("index.html", periodo=ALLOWED_YEARS, comunas=ALLOWED_CITIES)
+
+@app.route("/mapa")
+def show_data():
+
+    years = [int(year) for year in request.args.getlist("periodo")]
+    cities = [city + ', Chile' for city in request.args.getlist("comunas")]
+
+    client = MongoClient(MONGO_DB)
+    db = client[MONGO_CP_DB]
+    collection = db[CP_STRAVA_COLLECTION]
+    center = get_polygon_middle_point(cities[0], collection)
+    m = color_ride_map(cities[0], center, years, collection)
+
+    return m.get_root().render()
+
+
+@app.route("/fullscreen")
 def fullscreen():
-    # if request.method == 'POST':
-    #     city = request.form['city']
-    # else:
-    #     city = request.args.get('city', 'Viña del Mar')
-    # city += ', Chile'
     client = MongoClient(MONGO_DB)
     db = client[MONGO_CP_DB]
     collection1 = db[CP_STRAVA_COLLECTION]
     collection2 = db['ascensores']
     center = get_polygon_middle_point(city, collection1)
-    m = color_ride_map(city, center, [2019], collection1, collection2)
+    m = color_ride_map2(city, center, [2019], collection1, collection2)
     return m.get_root().render()
-
-
-# @app.route("/search")
-# def search():
-#     return render_template('search.html', cities=ALLOWED_CITIES)
-
 
 if __name__ == '__main__':
     if (DEBUG):
